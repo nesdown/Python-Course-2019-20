3cc3c896f13a574bdaa5d60f9d3aaf31789b0a3c
nesdown <glushko.bohdan@gmail.com>
2019-09-01 16:00:04 +0300

    Add first lesson

---
+++ 01_09_19/datatypes.py
@@ -0,0 +1,66 @@
+from time import sleep
+
+# INTEGER
+# decimal system
+a = 32
+print(42)
+
+# binary
+print(0b10)
+print(type(0b10))
+print(0b0010001)
+print(0b100010001100010111111)
+
+# octal
+print(0o10)
+print(type(0o10))
+print(0o1000110)
+
+# hex
+print(0x10)
+print(type(0x10))
+print(0x10001)
+
+# FLOAT
+print(type(4.2))
+print(type(4.))
+print(type(.4))
+
+print(4.2e-3)
+
+# IEEE754
+# 1.8 * 10^308
+# inf
+print(1.7976e308)
+print(1.8e308)
+
+# 5.0 * 10^-324
+print(5e-324)
+print(1e-324)
+
+# COMPLEX NUMBERS
+print(type(2+3j))
+
+# STRING
+print('She said: "Hello!"')
+
+# Escape Characters
+print('This string contains a single-quote () character.')
+ # \newline
+# a = input('Enter your data \n')
+print('jlfghkdlfjgklfdjgklfdjglkdfjglkdfjglkdfglkfdjglkfd\
+      dklfjgkldfjklgjdfklgjdflkgjdfklgjdfklgjdflkgjdfkljdklf\
+      fjsdklgjdklfjgkldfjgkldfjgkldfjgkldfjklgjklfdjgklfdjklfdg')
+
+print("Hello Worl\bd! \f \n \r \t \v")
+
+print('foo\nbar')
+print(r'foo\nbar')
+print('foo\\bar')
+print(R'foo\\bar')
+
+print('''Hello \ How are you \n I am fine 'very well' ''')
+
+# BOOLEAN
+print(type(True))
+print(type(False))
---
+++ 01_09_19/variables.py
@@ -0,0 +1,33 @@
+n = 300
+n = "Hello"
+
+# Simultaneous application
+a = b = c = 300
+print(a, b, c)
+
+print(300)
+
+# Creates an integer object
+# Gives a value 300
+# Displays to a console
+
+print(type(300))
+
+a = 3000000000032423423234423423423432234
+b = 3000000000032423423234423423423432234
+
+print(id(a))
+print(id(b))
+
+# [-5, 256]
+# PEP 8
+
+employeenumber = 4398
+
+EmployeeNumber = 4398
+
+employeeNumber = 4398
+
+print(id(employeenumber))
+print(id(EmployeeNumber))
+print(id(employeeNumber))
